252a679990f47ed062db8493c71b3e69c3ba2756
Mikenin-Ev <ormembar7@gmail.com>
2025-12-18 09:34:09 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte byteVar = 127;
+        System.out.println("Задача № 1 ");
+        System.out.println(" ");
+        System.out.println("Значение переменной byteVar c типом byte равно " + byteVar);
+        short shortVar = 30000;
+        System.out.println("Значение переменной shortVar c типом byte равно " + shortVar);
+        int intVar = 200000;
+        System.out.println("Значение переменной intVar c типом int равно " + intVar);
+        long longVar = 3000000L;
+        System.out.println("Значение переменной longVar c типом long равно " + longVar);
+        float floatVar = 35.2356f;
+        System.out.println("Значение переменной floatVar c типом float равно " + floatVar);
+        double doubleVar = 35.235678967;
+        System.out.println("Значение переменной doubleVar c типом double равно " + doubleVar);
+        System.out.println(" ");
+
+    }
+}
